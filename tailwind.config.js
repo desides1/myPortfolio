17d288b453022f5e916d30db9e6adb26ed569aa4
desides1 <desiayuth@gmail.com>
2024-08-27 16:48:47 +0700

    rename file index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./view/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  content: ['./docs/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
 
   theme: {
     container: {
