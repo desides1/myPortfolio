a0b4269e60b4d96f82a91989f0dd90dbf2214a08
desides1 <desiayuth@gmail.com>
2024-08-28 22:01:00 +0700

    add slider skill section and add sertifikat file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./docs/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  content: ['./view/**/*.{html,js}', './node_modules/flowbite/**/*.js', 'index.html'],
 
   theme: {
     container: {
@@ -15,6 +15,15 @@ module.exports = {
       },
       screens: {
         '2xl': '1320px',
+      },
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
       },
     },
   },
